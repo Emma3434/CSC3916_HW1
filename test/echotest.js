1422735b00514f80401e367fbc432c297000ef36
Emma3434 <51844300+Emma3434@users.noreply.github.com>
2020-02-15 00:12:29 -0700

    changed to echo_body

--- test/echotest.js
+++ test/echotest.js
@@ -6,6 +6,8 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 
+let echo_body = "emma";
+
 describe('Echo', () => {
     beforeEach((done) => { //Before each
             done();
@@ -15,10 +17,10 @@ describe('Echo', () => {
         it('it should POST echo', (done) => {
             chai.request(server)
                 .post('/')
-                .send('hello world')
+                .send(echo_body)
                 .end((err, res) => {
                     res.should.have.status(200);
-                    res.text.should.eq('hello world');
+                    res.text.should.eq(echo_body);
                     done();
                 });
         });
